33e4779b37c5acc0517954459b7ee602c7ff7939
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2024-06-04 15:37:10 +0400

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,13 +8,18 @@ from requests import get
 def top_ten(subreddit):
     """get first 10 hot post for a subreddit"""
     if subreddit and type(subreddit) is str:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
+        url = 'https://www.reddit.com/r/{}/hot.json\
+            '.format(subreddit)
+        headers = {'user-agent':
+                   'my-app/0.0.1'}
         params = {'limit': 10}
-        req = get(url, params=params, headers=headers, allow_redirects=False)
+        req = get(url, params=params,
+                  headers=headers, allow_redirects=False)
         if req.status_code == 200:
             data = req.json()
-            posts = data.get('data', {}).get('children', {})
+            # print(data)
+            posts = data.get('data', {})\
+                .get('children', {})
             for post in posts:
                 print(post.get('data').get('title'))
         else:
