3d17ed7662cac8ea602b5717f290ea8ec692f414
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2024-08-17 12:33:08 +0400

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -23,15 +23,13 @@ def count_words(subreddit, word_list, after=None, counts={}):
         base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     else:
         base_url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
-            subreddit, after
-        )
+            subreddit, after)
 
     headers = {'User-Agent': 'Agent Uche'}
     params = {'after': after} if after else {}
 
     response = requests.get(
-        base_url, headers=headers, params=params, allow_redirects=False
-    )
+        base_url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code != 200:
         return
@@ -43,11 +41,7 @@ def count_words(subreddit, word_list, after=None, counts={}):
         title = post['data']['title'].lower()
         for keyword in word_list:
             keyword = keyword.lower()
-            if (
-                keyword in title and not title.startswith(keyword + '.')
-                and not title.startswith(keyword + '!')
-                and not title.startswith(keyword + '_')
-            ):
+            if (keyword in title and not title.startswith(keyword + '.') and not title.startswith(keyword + '!') and not title.startswith(keyword + '_')):
                 counts[keyword] = counts.get(keyword, 0) + 1
 
     next_page = data.get('data', {}).get('after')
@@ -55,7 +49,6 @@ def count_words(subreddit, word_list, after=None, counts={}):
         count_words(subreddit, word_list, after=next_page, counts=counts)
     else:
         sorted_counts = sorted(
-            counts.items(), key=lambda item: (-item[1], item[0])
-        )
+            counts.items(), key=lambda item: (-item[1], item[0]))
         for word, count in sorted_counts:
             print("{}: {}".format(word, count))
