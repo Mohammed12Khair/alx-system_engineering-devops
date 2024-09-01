3d9667ba9b0ac7eb4d2a571c256013f3f8bbab46
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2024-09-01 18:51:39 +0400

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and returns the number of subscribers """
+"""Quering Reddit"""
+
 import requests
 
 
-def number_of_subscribers(subreddit) -> int:
-    """
-    Queries the Reddit API and returns the number of subscribers
+def number_of_subscribers(subreddit):
+    """query a subreddit and retrive no of subscribers"""
 
-    Args:
-        subreddit (str): subreddit to query
+    # Reddit API endpoint for getting subreddit informatiom
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    Returns:
-        (int): number of subscribers
-    """
-    # Returns the number of subscribers for a given subreddit
-    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    try:
-        request = requests.get(base_url, headers={'User-Agent': 'Agent Uche'})
-        data = request.json()
-        # print(data['data']['subscribers'])
-        return data['data']['subscribers']
-    except Exception:
+    # Set a custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'My user Agent 1.0'}
+
+    # send a GET request to the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Chek if the request was successful and not redirect
+    if response.status_code == 200:
+        # parse JSON response to extract no of subscribers
+        data = response.json().get('data', {})
+        sub_count = data.get('subscribers', 0)
+        return sub_count
+    else:
         return 0
